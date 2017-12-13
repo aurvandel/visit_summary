3e25633be0b8bf258f4eb2f90f0e3fd17a4d07fb
Parker Watkins <parkergw@gmail.com>
2017-12-12 17:30:08 -0700

    Autocopy works for 1 provider

--- VisitSummaryV2_auto_copy.py
+++ VisitSummaryV2_auto_copy.py
@@ -10,35 +10,47 @@ python-docx, tkinter and win32api. Will automatically copy the data required to 
 
 '''
 
-import ctypes
 import os
 import string
 import tkinter
 import tkinter.messagebox
+import time
 
 import docx
 import docx.enum.text
 import docx.shared
-import windowmgr
-import pyautogui
+import pywinauto
 
 import win32api
 
-
-#TODO Automatically select all from PowerChart window and copy
 #TODO Make the loop to print out all of the pages at once
+#TODO Find better why to get the date
 
 CWD = os.getcwd()
 
 def copy_from_icentra():
-    w = windowmgr.WindowMgr()
-    w.find_window_wildcard("PowerChart")
-    w.set_foreground()
-    w.set_focus()
-    pyautogui.click(821, 233)
-    pyautogui.hotkey('ctrl', 'a')
-    pyautogui.hotkey('ctrl', 'c')
-    return
+
+    '''Automatically select the PowerChart window then select all and copy to clipboard'''
+    clipboard = []
+    w_handle = pywinauto.findwindows.find_window(title_re="PowerChart Organizer for")
+    app = pywinauto.application.Application().connect(handle=w_handle)
+    window = app.window(handle=w_handle)
+    window.Maximize()
+    window.SetFocus()
+    window.set_keyboard_focus()
+    window.ClickInput(coords=(141, 316))
+    window.DoubleClick(coords=(141, 316))
+    #window.DoubleClick(coords=(141, 316))
+    #window.Wait('active').TypeKeys('^a')
+    window.Wait('active').TypeKeys('^c')
+    time.sleep(1)                               # have to wait for the clipboard to fill up
+    clipboard = tkinter.Tk().clipboard_get()    # copy contents of clipboard to get date
+    window.ClickInput(coords=(640, 339))
+    window.TypeKeys('^a')
+    window.TypeKeys('^c')
+    time.sleep(1)
+    clipboard += tkinter.Tk().clipboard_get()   # copy contents of clipboard to get patients
+    return clipboard
 
 def import_clip_board():
 
@@ -62,8 +74,7 @@ def import_clip_board():
         'November' : '11',
         'December' : '12',
         }
-    copy_from_icentra()
-    clipboard = tkinter.Tk().clipboard_get()  # copy contents of clipboard
+    clipboard = copy_from_icentra()
     provider = ''
     lst = clipboard.split()
     patient_list = []
@@ -76,12 +87,14 @@ def import_clip_board():
             provider = 'Jennifer Fisher, DNP'
         elif lst[i] == 'MD,' and lst[i + 1] == 'Kirk':
             provider = 'Kirk Watkins, MD'
-        elif lst[i] == 'DXSD':
+        elif lst[i] == 'DXSD' and lst[i + 1] == 'PUL':
             provider = 'Josh Conner, CRT, RPSGT'
         elif lst[i] in months:
             day_date = lst[i + 1]
             new_day = day_date.translate(table)
             date = months[lst[i]] + '/' + new_day + '/' + lst[i + 2]
+        elif '---' in lst[i]:
+            return patient_list, provider, date
         elif lst[i].isupper() and lst[i].endswith(','):
             if lst[i] == 'JR,' or lst[i] == 'SR,' or lst[i] == 'III,':
                 patient_list.append(lst[i - 1] + ', ' + lst[i + 1])
@@ -107,7 +120,7 @@ def create_document(patients, day, provider):
     doc._body.clear_content()
     for i in range(len(patients)):
         table = doc.add_table(rows=3, cols=2)
-        table.style = 'TableGrid'
+        table.style = 'Table Grid'
         a = table.cell(0, 1)
         b = table.cell(2, 1)
         logo_cell = a.merge(b)
@@ -122,12 +135,23 @@ def create_document(patients, day, provider):
         date_cell.text = "Date of Visit: " + day
         paragraph = logo_cell.paragraphs[0]
         paragraph_format = paragraph.paragraph_format
-        paragraph_format.alignment = docx.enum.text.WD_PARAGRAPH_ALIGNMENT.RIGHT
+        paragraph_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.RIGHT
         run = paragraph.add_run()
         drsdc_logo_path = os.path.join(CWD, "DRSDC_V_3CPT.bmp")
         run.add_picture(drsdc_logo_path, height=docx.shared.Inches(1.0))
         aasm_logo_path = os.path.join(CWD, "Accredited Center logo.bmp")
         run.add_picture(aasm_logo_path, height=docx.shared.Inches(0.5))
+
+        # obj_styles = doc.styles
+        # obj_charstyle = obj_styles.add_style('TitleStyle', docx.enum.style.WD_STYLE_TYPE.CHARACTER)
+        # obj_font = obj_charstyle.font
+        # obj_font.size = docx.shared.Pt(18)
+        
+        # heading = doc.add_paragraph('Visit Summary')
+        # heading_format = heading.paragraph_format
+        # heading_format.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+        # heading_format.space_before = docx.shared.Pt(12)
+
         if i == len(patients) - 1:
             pass
         else:
