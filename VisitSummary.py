bfb64bbabfcc906924cfeab7148937e7221b2e87
Parker Watkins <parkergw@gmail.com>
2018-01-19 16:34:10 -0700

    update

--- VisitSummary.py
+++ VisitSummary.py
@@ -1,14 +1,18 @@
 #!python27
 
-''' Schedule Scrapper and Visit Summary Generator
+"""
+Schedule Scrapper and Visit Summary Generator
 
 Simple script to allow the schedular to copy a providers schedule and create a word
 document showing the patient's name, date of visit and the provider's name. It's then used
 as a "scratch pad" for any instructions that the provider would like the patient to remember.
-It's written in python 3 and uses py2exe to "compile" it for use on Windows systems. It uses
-python-docx, tkinter and win32api. Will automatically copy the data required to the clipboard
+It's written in python 2 and uses py2exe to "compile" it for use on Windows systems. It uses
+python-docx, tkinter, dateutile, pywinauto and win32api. Will automatically copy the data
+required to the clipboard, save the data to a Word template and print the newly created
+document.
 
-'''
+"""
+#TODO admin button that opens a txt file to change holidays
 
 import datetime
 import operator
@@ -30,7 +34,7 @@ CWD = os.getcwd()
 
 class InputDate(object):
 
-    ''' tkinter window to input the date as a string'''
+    """ tkinter window to input the date as a string"""
 
     def __init__(self, requestMessage):
         self.font = ("Helvetica", 14)
@@ -59,13 +63,14 @@ class InputDate(object):
         quit_button.place(x=330, y=120)
         self.root.protocol("WM_DELETE_WINDOW", lambda: sys.exit())
         self.acceptInput(requestMessage)
+        self.mHolidays = self.get_holidays()
 
     def acceptInput(self, requestMessage):
-        ''' Creates tkinter labels and entry box'''
+        """ Creates tkinter labels and entry box"""
         r = self.frame
-        instructions = Tkinter.Label(r, text='Please have iCentra\'s Ambulatory Organizer window open.\
- For tomorrow\'s schedule click Tomorrow, otherwise, input the date of the schedule you\'re working on\
- and click OK.', wraplength=400)
+        instructions = Tkinter.Label(r, text="Please have iCentra's Ambulatory Organizer window open and on the correct date.\
+ Click Next Business Day or input the date of the schedule you're working on\
+ and click OK.", wraplength=400)
         instructions.pack(side='top', pady=5)
         k = Tkinter.Label(r, text=requestMessage, font=self.font, padx=5)
         k.pack(side='left', pady=5)
@@ -74,34 +79,26 @@ class InputDate(object):
         self.e.focus_set()
 
     def tomorrow(self):
-        '''Gets tommorows date'''
+        """Gets tomorrow's date"""
         date = datetime.date.today() + datetime.timedelta(days=1)
         self.string = date.strftime('%m/%d/%Y')
         self.root.destroy()
 
+    def get_holidays(self):
+        """Gets list of holidays from file"""
+        with open('holidays.txt') as fin:
+            holidays = [line.rstrip('\n') for line in fin]
+        holidays = holidays[5:]
+        format_holidays = [datetime.datetime.strptime(x, '%m/%d/%Y') for x in holidays]
+        return format_holidays
+
     def next_business_day(self):
-        '''Returns the next business day'''
-        holidays = [
-            datetime.datetime(2017, 12, 24,),
-            datetime.datetime(2017, 12, 25,),
-            datetime.datetime(2017, 12, 31,),
-            datetime.datetime(2018, 1, 1,),
-            datetime.datetime(2018, 1, 15,),
-            datetime.datetime(2018, 2, 19,),
-            datetime.datetime(2018, 5, 28,),
-            datetime.datetime(2018, 7, 4,),
-            datetime.datetime(2018, 7, 24,),
-            datetime.datetime(2018, 9, 3,),
-            datetime.datetime(2018, 11, 22,),
-            datetime.datetime(2018, 12, 24,),
-            datetime.datetime(2018, 12, 25,),
-            datetime.datetime(2019, 1, 1,),
-            ]
+        """Returns the next business day"""
 
         # Create a rule to recur every weekday starting today
         r = rrule.rrule(rrule.DAILY,
                         byweekday=[rrule.MO, rrule.TU, rrule.WE, rrule.TH, rrule.FR],
-                        dtstart=datetime.date.today())
+                        dtstart=datetime.date.today() + datetime.timedelta(days=1))
 
         # Create a rruleset
         rs = rrule.rruleset()
@@ -110,36 +107,68 @@ class InputDate(object):
         rs.rrule(r)
 
         # Add holidays as exclusion days
-        for exdate in holidays:
+        for exdate in self.mHolidays:
             rs.exdate(exdate)
         # Remove time from date and reformat to human preferred date
-        nbd = rs[0] + datetime.timedelta(days=1)
+        nbd = rs[0]
         self.string = nbd.strftime('%m/%d/%Y')
         self.root.destroy()
 
     def gettext(self):
-        '''Used to get user input from tkinter entry box'''
+        """Used to get user input from tkinter entry box"""
         self.string = self.e.get()
         self.root.destroy()
 
     def getString(self):
-        '''Getter method to return the user input'''
+        """Getter method to return the user input"""
         return self.string
 
     def waitForInput(self):
-        '''loops through tkinter window until button is clicked'''
+        """loops through tkinter window until button is clicked"""
         self.root.mainloop()
 
+class AllDoneMsgBox(InputDate):
+    def __init__(self):
+        """Message box to alert the user that the papers have been printed"""
+        self.font = ("Helvetica", 14)
+        self.root = Tkinter.Tk()
+        # Centers the window
+        self.root.wm_attributes("-topmost", 1)
+        self.root.update_idletasks()
+        width = self.root.winfo_width()
+        height = self.root.winfo_height()
+        x = (self.root.winfo_screenwidth() // 2) - (width // 2)
+        y = (self.root.winfo_screenheight() // 2) - (height // 2)
+        self.root.geometry('{}x{}+{}+{}'.format(380, 90, x, y))
+        self.root.wm_iconbitmap("logo_icon.ico")
+        self.root.title('Visit Summary Generator')
+        self.msg = "Congratulations!\n Visit Summaries have all printed."
+        self.duration = 10000
+        self.w = Tkinter.Label(self.root, text=self.msg, font=self.font, wraplength=400)
+        self.w.pack()
+        self.label = Tkinter.Label(self.root, text="")
+        self.label.pack()
+        self.remaining = 0
+        self.countdown(10)
+        self.root.after(self.duration, self.root.destroy)
+        self.root.protocol("WM_DELETE_WINDOW", lambda: sys.exit())
+
+    def countdown(self, remaining=None):
+        if remaining is not None:
+            self.remaining = remaining
+        self.label.configure(text="This window will close in %d seconds" % self.remaining)
+        self.remaining = self.remaining - 1
+        self.root.after(1000, self.countdown)
 
 def getDate(requestMessage):
-    '''creates tkinter window to get user input'''
+    """creates tkinter window to get user input"""
     msgBox = InputDate(requestMessage)
     #loop until the user makes a decision and the window is destroyed
     msgBox.waitForInput()
     return msgBox.getString()
 
 def confirm_date(date):
-    '''creates tkinter window to confirm date'''
+    """creates tkinter window to confirm date"""
     msg = "Is {0} the correct date?".format(date)
     root = Tkinter.Tk()
     root.withdraw()
@@ -151,7 +180,7 @@ def confirm_date(date):
         return False
 
 def no_powerchart_error():
-    '''Error window shown if PowerChart is not open'''
+    """Error window shown if PowerChart is not open"""
     msg = "PowerChart is not open. Please open PowerChart and click Retry to continue or Cancel to quit"
     root = Tkinter.Tk()
     root.withdraw()
@@ -162,7 +191,7 @@ def no_powerchart_error():
         sys.exit()
 
 def empty_clipboard_error():
-    '''Error window shown if the clipboard is empty'''
+    """Error window shown if the clipboard is empty"""
     msg = "The schedule did not copy correctly. Please make sure that PowerChart is open to the schedule and click Retry to continue or Cancel to quit"
     root = Tkinter.Tk()
     root.withdraw()
@@ -174,9 +203,8 @@ def empty_clipboard_error():
 
 def copy_from_icentra():
 
-    '''Automatically select the PowerChart window then select all and copy to clipboard'''
+    """Automatically select the PowerChart window then select all and copy to clipboard"""
 
-    clipboard = []
     try:
         w_handle = pywinauto.findwindows.find_window(title_re="PowerChart Organizer for")
         app = pywinauto.application.Application().connect(handle=w_handle)
@@ -190,15 +218,17 @@ def copy_from_icentra():
     except pywinauto.findwindows.WindowNotFoundError:
         no_powerchart_error()
     time.sleep(1)                               # have to wait for the clipboard to fill up
-    clipboard = Tkinter.Tk().clipboard_get()    # copy contents of clipboard
+    root = Tkinter.Tk()
+    clipboard = root.clipboard_get()    # copy contents of clipboard
+    root.destroy()
     if len(clipboard) == 0:
         empty_clipboard_error()
     return clipboard
 
 def import_clip_board():
 
-    '''Imports the contents of the clipboard, strips it of unneeded values and returns a
-    list of tuples, (provider, patient)'''
+    """Imports the contents of the clipboard, strips it of unneeded values and returns a
+    list of tuples, (provider, patient)"""
 
     exclusions = [
         'JR,',
@@ -206,11 +236,7 @@ def import_clip_board():
         'II,',
         'III,'
     ]
-    provider = ''
-    patient = ''
-    new_lst = []
     final_lst = []
-    line_list = []
     time_re = re.compile(r'^([0-1]?[0-9]|[2][0-3]):([0-5][0-9])$')
     clipboard = copy_from_icentra()
     lst = clipboard.split()
@@ -228,7 +254,6 @@ def import_clip_board():
                 for j in range(len(line_list)):
                     if 'No' in line_list and 'appointments' in line_list:
                         break
-                    provider, patient = '', ''
                     if line_list[j] == 'APRN,':
                         provider = "Mark Boyer, FNP"
                         if line_list[j + 6] in exclusions:
@@ -264,13 +289,12 @@ def import_clip_board():
                         else:
                             patient = str(line_list[j + 6]) + ' ' + str(line_list[j + 7])
                         final_lst.append((provider, patient))
-                line_list = []
     final_lst = sorted(final_lst, key=operator.itemgetter(0))
     return final_lst
 
 def create_document(schedule, day):
 
-    '''Uses docx to create a word document using the schedule tuple, (provider, patient)'''
+    """Uses docx to create a word document using the schedule tuple, (provider, patient)"""
 
     excluded = ["'", '(', ')']
     day = str(day)
@@ -324,7 +348,7 @@ def create_document(schedule, day):
     doc.save(save_path)
 
 def print_word_document(filename):
-    '''Opens the document in Word, prints it and closes Word'''
+    """Opens the document in Word, prints it and closes Word"""
     word = client.Dispatch("Word.Application")
     word.Documents.Open(filename)
     word.ActiveDocument.PrintOut()
@@ -332,26 +356,24 @@ def print_word_document(filename):
     word.ActiveDocument.Close()
     word.Quit()
 
-def all_done_msgbox():
-    '''Message box to alert the user that the papers have been printed'''
-    root = Tkinter.Tk()
-    root.withdraw()
-    tkMessageBox.showinfo("Visit Summary", "Congratulations! Visit Summaries have all printed.")
-    return
-
 # def delete_paragraph(paragraph):
-#     '''Delete a specific paragraph, currently not used'''
+#     """Delete a specific paragraph, currently not used"""
 #     p = paragraph._element
 #     p.getparent().remove(p)
 #     p._p = p._element = None
 
+def done():
+    box = AllDoneMsgBox()
+    box.waitForInput()
+
 if __name__ == "__main__":
     flag = False
+    day = datetime.date.today()
     while not flag:
         day = getDate('Schedule date')
         flag = confirm_date(day)
     path = os.path.join(CWD, "patient.docx")
     provider_patient = import_clip_board()
     create_document(provider_patient, day)
-    print_word_document(path)
-    all_done_msgbox()
+    #print_word_document(path)
+    done()
