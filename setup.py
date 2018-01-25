8f5ce3918a57b5c4031c1d59fadfecae3fc32de7
Parker Watkins <parkergw@gmail.com>
2018-01-25 12:25:04 -0700

    finished version 2

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from cx_Freeze import setup, Executable
+import os.path
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
 added_files = ['Accredited Center logo.bmp', 'DRSDC and AASM.bmp', 'DRSDC_V_3CPT.bmp', 'holidays.txt', 'logo_icon.ico',
-               'patient.docx', 'template.docx']
+               'patient.docx', 'template.docx',
+               os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+               os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+               ]
 
 buildOptions = dict(packages=[], excludes=[], include_files=added_files)
 
